23dfe8f791410cf6ca277cb8ba2e10d552b26206
Anthony Hughes <tony2nite@gmail.com>
2013-04-22 20:55:20 +0100

    Make jokes scroll over time

--- app/views/home_view.js
+++ app/views/home_view.js
@@ -5,16 +5,37 @@ module.exports = View.extend({
   id: 'home-view',
   template: template,
 
+  jokeIndex: 0,
+  DISPLAY_TIME: 5000,
+
   initialize: function (options) {
   	console.log('binding', this.collection);
     _.bindAll(this, "render");
     this.collection.bind('reset', this.render);
+
+    $(this.el).css({display: "none"});
   },
 
+
+
   render: function() {
-  	var data = this.collection.toJSON();
-  	console.log('render', data);
-	this.el.innerHTML = template({jokes: data});
+  	var self = this;
+
+
+  	var joke = this.collection.at(this.jokeIndex);
+	this.el.innerHTML = template({joke: joke.toJSON()});
+
+	var increment = function () {
+		self.jokeIndex++;
+		if (self.jokeIndex == self.collection)
+			self.jokeIndex = 0;
+		self.render();
+	};
+
+	$(this.el)
+		.fadeIn()
+		.delay(self.DISPLAY_TIME)
+		.fadeOut('slow', function() {increment();})
   	return this;
   }
 
